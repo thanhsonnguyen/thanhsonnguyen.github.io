cbe834d3598cbdc150c8ca724340b75ae4874bfa
Sourav <srvz39@gmail.com>
2021-02-02 19:00:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+
+
+const input_config = fs.readFileSync('./input_config.json', {encoding:'utf8', flag:'r'})
+const chart_ids = fs.readFileSync('./chart_ids.json', {encoding:'utf8', flag:'r'})
+
+console.log(input_config)
+console.log(chart_ids)
