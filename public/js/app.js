e24eb18eeef22ded7e499eb16a6cc3d1f51b6b8b
Sourav <srvz39@gmail.com>
2021-02-03 23:15:31 +0530

    basic PWA now working

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,26 @@
 const container = document.querySelector(".container");
 
-const showGraphs = ()=>{
+const showGraphs = () => {
+  document.querySelector('#searchBtn').addEventListener("click", function () {
+    serial_no = document.getElementById('serial_no_input').value
 
+    fetch("../public/generated_html_files/" + serial_no + ".html" /*, options */ )
+      .then((response) => response.text())
+      .then((html) => {
+        // document.getElementById("container").innerHTML = html
+        document.querySelector(".container").innerHTML = html
+      })
+      .catch((error) => {
+        console.warn(error)
+      })
+  })
 }
 
 
 document.addEventListener("DOMContentLoaded", showGraphs);
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
       .then(res => console.log("service worker registered"))
